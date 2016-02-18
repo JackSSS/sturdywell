3f73218d664b20f1f9f66d4b4076c1b85ca09ea4
Jack <sneed.jack@gmail.com>
2016-02-18 09:44:12 -0800

    add angular

---
+++ server.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var appRouter = require(__dirname + '/routes/app_routes');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/sturdywell');
+
+app.use(express.static('build'));
+
+app.use('/api', appRouter);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server up');
+});
